cf8e2a8e73f2b6675ba944a1ac4f8a3ec43cbe22
TencV-tys <vincenttayros078@gmail.com>
2025-09-03 18:45:26 +0800

    midterm

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-
-import {SafeAreaView,StyleSheet} from 'react-native';
+import React,{useState} from "react";
+import {SafeAreaView,StyleSheet,View,
+  Text,TextInput,FlatList
+} from 'react-native';
 
 
 export default function App() {
 
   return (
  <SafeAreaView style={styles.container}>
- 
+   
+   
+   
+   
  </SafeAreaView>
    )
 }
