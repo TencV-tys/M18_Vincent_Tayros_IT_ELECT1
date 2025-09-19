1c77e7c099ba314d59fcd0aabd19b28dbaf88609
TencV-tys <vincenttayros078@gmail.com>
2025-09-19 21:05:12 +0800

    latest

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React,{useState} from "react";
-import {SafeAreaView,StyleSheet,View,
-  Text,TextInput,FlatList
+import {SafeAreaView,StyleSheet,Button,View
 } from 'react-native';
-
-
+import ChatScreen from "./ChatScreen.js";
+import CommentInput from "./CommentInput.js";
 export default function App() {
+  const [ page,setPage]=useState("chat");
+
 
   return (
  <SafeAreaView style={styles.container}>
-   
-   
-   
-   
+ {page === "chat" &&<ChatScreen/>}
+ {page === "comment"&&<CommentInput/>}
+  <View style={styles.buttons}>
+  <Button title="Go to Chat"
+  onPress={()=>setPage("chat")}/>
+  <Button title="Go to Comment"
+  onPress={()=>setPage("comment")}/>
+  </View>
  </SafeAreaView>
    )
 }
@@ -19,5 +24,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
     flex:1
+  },
+  buttons:{
+    flex:1
   }
+  
 });
